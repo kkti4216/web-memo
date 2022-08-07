917e5a026c23c714a296c7e0780629b409f23dd5
kkti4216 <kkti4216@gmail.com>
2022-08-07 16:26:34 +0900

    bootstrap

--- flask/app.py
+++ flask/app.py
@@ -1,20 +1,18 @@
+from ensurepip import bootstrap
 from flask import *
 import sqlite3
+import flask_bootstrap
 from datetime import datetime
-from config import * 
+from config import *
 
 app = Flask(__name__)
+bootstrap = flask_bootstrap.Bootstrap(app)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def main():
     con = sqlite3.connect(DATABASE)
     con.row_factory = sqlite3.Row
-    
-    if request.method == "POST":
-        item = {"time":datetime.now().isoformat(timespec='minutes'), "text":request.form["new_rec"]}
-        if item["text"] != "":
-            con.execute("insert into memo(time,text) values(:time,:text)",item)
-    
+
     res = con.execute("select * from memo")
     memo = []
     while True:
@@ -27,6 +25,22 @@ def main():
     con.close()
 
     return render_template("index.html", memo=memo)
+
+@app.route("/", methods=["POST"])
+def post():
+    con = sqlite3.connect(DATABASE)
+    con.row_factory = sqlite3.Row
+
+    item = {"time":datetime.now().isoformat(timespec='minutes'), "text":request.form["new_rec"]}
+    if item["text"] != "":
+        con.execute("insert into memo(time,text) values(:time,:text)",item)
+
+    con.commit()
+    con.close()
+
+    return redirect("/")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
